0ff07b93b66336e7c4dfc4c2dd5adce89984d0e2
johnmendez <john.a.mendez2012@gmail.com>
2017-02-27 12:37:37 -0600

    Testing

---
+++ app/reducer.js
@@ -0,0 +1,12 @@
+export default function reducer(state, action) {
+  switch (action.type) {
+    case 'CONTACT@FIND_ALL':
+      return { contacts: action.data };
+
+    case 'CONTACT@CREATE':
+      return { contacts: [action.data] };
+
+    default:
+      return state || { contacts: [] };
+  }
+}
---
+++ tests/reducer-test.js
@@ -0,0 +1,27 @@
+import reducer from '../app/reducer';
+
+module('reducer', () => {
+  test('it exists', (assert) => {
+    assert.ok(reducer, 'reducer exists');
+  });
+
+  test('Load All Full Names', (assert) => {
+    const oldState = { contacts: [] };
+    const oldStateTwo = { contacts: [{ firstName: 'Angelina', lastName: 'Jolie' }] };
+    const actionOne = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'John', lastName: 'Cena' }] };
+    const actionTwo = { type: 'CONTACT@FIND_ALL', data: [{ firstName: 'John', lastName: 'Cena' }] };
+
+    assert.deepEqual(reducer(oldState, actionOne), { contacts: [{ firstName: 'John', lastName: 'Cena' }] });
+    assert.deepEqual(reducer(oldStateTwo, actionTwo), { contacts: [{ firstName: 'John', lastName: 'Cena' }] });
+  });
+
+  test('Add A Full Name', (assert) => {
+    const oldState = { contacts: [] };
+    const oldStateTwo = { contacts: [{ firstName: 'Angelina', lastName: 'Joloe' }] };
+    const actionOne = { type: 'CONTACT@CREATE', data: { firstName: 'Johnny', lastName: 'Depp' } };
+    const actionTwo = { type: 'CONTACT@CREATE', data: { firstName: 'Johnny', lastName: 'Depp' } };
+
+    assert.deepEqual(reducer(oldState, actionOne), { contacts: [{ firstName: 'Johnny', lastName: 'Depp' }] });
+    assert.deepEqual(reducer(oldStateTwo, actionTwo), { contacts: [{ firstName: 'Johnny', lastName: 'Depp' }] });
+  });
+});
